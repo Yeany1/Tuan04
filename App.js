10da41ac0cdeb0bf3535a02071fee8a5a7d3f03a
khanh260802 <dinhbaoyeny2002@gmail.com>
2023-10-18 02:41:39 +0700

    fix loi

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import FirstScreen from './screens/FirstScreen';
+import A from './screens/A';
+import B from './screens/B';
+import C from './screens/C';
+import E from './screens/E';
+import D from './screens/D';
+import A2 from './screens/A2';
+import XMEye from './screens/XMEye';
 
 export default function App() {
+  console.log(FirstScreen)
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+      {/* <FirstScreen /> */}
+      {/* <A /> */}
+      {/* <B /> */}
+      {/* <C /> */}
+      {/* <D /> */}
+      {/* <E /> */}
+      {/* <A2 /> */}
+      <XMEye />
     </View>
   );
 }
